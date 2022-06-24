6510c3e932d111bd3ad68ec57144560d1ca6721c
oziuzoma <oziuzoma@gmail.com>
2022-06-24 13:05:27 -0400

    createinfra

---
+++ .circleci/ansible/roles/configure-server/files/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y 
+sudo apt update -y
+sudo apt install python3.8 -y
+sudo apt install nodejs -y
+sudo apt install npm -y
+npm install pm2 -g -y
